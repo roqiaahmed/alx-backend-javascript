a6f563d10a1ed7756e4f628197db227fd40cbb56
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2024-01-02 17:15:26 +0200

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -4,11 +4,11 @@ export function taskFirst() {
 }
 
 export function getLast() {
-  return " is okay";
+  return ' is okay';
 }
 
 export function taskNext() {
-  let combination = "But sometimes let";
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;

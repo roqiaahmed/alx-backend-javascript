7887d883760c367efbf5dbc2f7ebd4a4deb21139
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-01-02 19:19:50 +0200

    lol

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
     const idx = array.indexOf(value);
--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,4 +1,6 @@
-import getBudgetObject from "./7-getBudgetObject.js";
+/* eslint-disable quotes */
+/* eslint-disable import/no-unresolved */
+import getBudgetObject from "7-getBudgetObject";
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);

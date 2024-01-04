e312adef9f0fc7df8f67d610673f7195d9556126
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2024-01-04 22:48:11 +0200

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,38 +1,40 @@
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    if (typeof amount !== "number") {
-      throw new TypeError("Amount must be a number");
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
     } else {
       this._amount = amount;
     }
     if (currency instanceof Currency) {
       this._currency = currency;
     } else {
-      throw new TypeError("Currency must be a Currency");
+      throw new TypeError('Currency must be a Currency');
     }
   }
 
   get amount() {
     return this._amount;
   }
+
   get currency() {
     return this._currency;
   }
 
   set amount(newAmount) {
-    if (typeof newAmount !== "number") {
-      throw new TypeError("Amount must be a number");
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
     } else {
       this._amount = newAmount;
     }
   }
+
   set currency(newCurrency) {
     if (newCurrency instanceof Currency) {
       this._currency = newCurrency;
     } else {
-      throw new TypeError("Currency must be a Currency");
+      throw new TypeError('Currency must be a Currency');
     }
   }
 
@@ -41,10 +43,10 @@ export default class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== "number") {
-      throw new TypeError("Amount must be a number");
-    } else if (typeof conversionRate !== "number") {
-      throw new TypeError("ConversionRate must be a number");
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    } else if (typeof conversionRate !== 'number') {
+      throw new TypeError('ConversionRate must be a number');
     } else {
       return amount * conversionRate;
     }

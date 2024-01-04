19937e69b72ac16d93c17c34a54d4dd48810bd6e
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2024-01-04 23:01:24 +0200

    Create 100-evcar.js

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,17 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species](){
+    return Car;
+  }
+
+  cloneCar(){
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+}

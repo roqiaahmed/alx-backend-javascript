3d11564b19f7be5b628a4c1bd0197b5341a985e3
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2024-01-04 22:39:32 +0200

    Update 10-car.js

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -8,6 +8,7 @@ export default class Car {
   static get [Symbol.species]() {
     return this;
   }
+
   cloneCar() {
     const Species = this.constructor[Symbol.species];
     return new Species();

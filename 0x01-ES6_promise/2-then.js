94192e15c76fddae432518a089b954218e014877
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2024-01-04 13:08:11 +0200

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,12 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return promise.then(
     () => {
-      console.log("Got a response from the API");
-      return { status: 200, body: "Success" };
+      console.log('Got a response from the API');
+      return { status: 200, body: 'Success' };
     },
     () => {
-      console.log("Got a response from the API");
-      return Error();
+      console.log('Got a response from the API');
+      return new Error();
     }
   );
 }

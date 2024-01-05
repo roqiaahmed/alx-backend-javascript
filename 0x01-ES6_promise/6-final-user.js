9e82e2f909c6dc4aae0d31edc8b5521c69427db2
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2024-01-05 14:46:14 +0200

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -12,7 +12,7 @@ export default async function handleProfileSignup(
   return Promise.allSettled([signUpPromise, uploadPromise]).then((results) => {
     results.map((result) => ({
       status: result.status,
-      value: result.status === "fulfilled" ? result.value : result.reason,
+      value: result.status === "fulfilled" ? result.value : String(result.reason),
     }));
   });
 }

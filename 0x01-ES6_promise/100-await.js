7a337e1f45208fd1348ba385c03c3162bea17ffc
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-01-05 15:55:32 +0200

    100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,18 +1,17 @@
 import { uploadPhoto, createUser } from "./utils";
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-
-  if (!photo && !user) {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    return {
+      photo,
+      user,
+    };
+  } catch (e) {
     return {
       photo: null,
       user: null,
     };
   }
-
-  return {
-    photo,
-    user,
-  };
 }

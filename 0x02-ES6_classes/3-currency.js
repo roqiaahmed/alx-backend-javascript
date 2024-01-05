92ddbcf439d7e607d5cc64e025290f6599e01d07
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2024-01-05 13:57:18 +0200

    Update 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -4,6 +4,14 @@ export default class Currency {
     this._name = name;
   }
 
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }

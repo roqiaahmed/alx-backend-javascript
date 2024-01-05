1eb508159a8341d24b680f9a24be1e3549e43dbf
Roqia Ahmed <103763199+roqiaahmed@users.noreply.github.com>
2024-01-05 14:51:57 +0200

    Update 7-load_balancer.js

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -2,6 +2,6 @@ export default function loadBalancer(chinaDownload, USDownload) {
   const chinaDownloadPromise = chinaDownload;
   const USDownloadPromise = USDownload;
   return Promise.race([chinaDownloadPromise, USDownloadPromise]).then(
-    (value) => value;
+    (value) => value
   );
 }

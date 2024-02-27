0bd736bb3383af800b188d8b5525d3d389aee151
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-27 20:12:59 +0200

    5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const { argv } = require('process');
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(argv[2])
+      .then((data) => {
+        res.end(data);
+      })
+      .catch(() => {
+        process.exit(1);
+      });
+  }
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+module.exports = app;

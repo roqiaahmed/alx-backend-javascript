6f77527a6190887c4da931a3c81afcaa1e391b2d
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-27 16:55:22 +0200

    3

--- 0x05-Node_JS_basic/0-main.js
+++ 0x05-Node_JS_basic/0-main.js
@@ -1,5 +1,10 @@
-const countStudents = require("./2-read_file");
-
-// countStudents("nope.csv");
-
-countStudents("database.csv");
+const countStudents = require("./3-read_file_async");
+countStudents("nope.csv")
+  // countStudents("database.csv")
+  .then(() => {
+    console.log("Done!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+console.log("After!");
---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-plusplus */
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((res, rej) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        rej(Error('Cannot load the database'));
+      } else {
+        const hashMap = new Map();
+        let num = 0;
+        let msg = '';
+
+        const arrData = data.split('\n');
+        for (let i = 1; i < arrData.length; i++) {
+          const line = arrData[i].split(',');
+          if (line.length === 4) {
+            const name = line[0];
+            const key = line[3].trim();
+
+            if (hashMap.has(key)) {
+              const list = hashMap.get(key);
+              list.push(name);
+              hashMap.set(key, list);
+              num += 1;
+            } else {
+              num += 1;
+              const list = [name];
+              hashMap.set(key, list);
+            }
+          }
+        }
+
+        console.log(`Number of students: ${num}`);
+        msg += `Number of students: ${num}`;
+        for (const [key, value] of hashMap.entries()) {
+          console.log(
+            `Number of students in ${key}: ${value.length}. List: ${value.join(
+              ', ',
+            )}`,
+          );
+          msg += `Number of students in ${key}: ${
+            value.length
+          }. List: ${value.join(', ')}`;
+        }
+        res(msg);
+      }
+    });
+  });
+}
+
+module.exports = countStudents;

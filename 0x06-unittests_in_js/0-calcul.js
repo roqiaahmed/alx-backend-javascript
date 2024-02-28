9581d3b07347660436de0cb952e0ba891d0b6118
Roqia Ahmed <roqiaahmed447@gmail.com>
2024-02-28 23:32:44 +0200

    init

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,6 @@
+function calculateNumber(a, b) {
+  let br = Math.round(b);
+  let ar = Math.round(a);
+  return br + ar;
+}
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,17 @@
+const assert = require("assert");
+const calculateNumber = require("./0-calcul");
+
+describe("calcul", () => {
+  it("checks round", () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it("checks round", () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it("checks round", () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it("checks round afds", () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+});
